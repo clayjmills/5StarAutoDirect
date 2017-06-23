5c2fc22a8ac20586c4239dce976f2fb3e3e3655c
witty89 <witty89@gmail.com>
2017-06-23 12:15:55 -0600

    added firebaseCore pod

--- 5StarAutoDirect/SignUpViewController.swift
+++ 5StarAutoDirect/SignUpViewController.swift
@@ -7,24 +7,27 @@
 //
 
 import UIKit
-import Firebase
+import FirebaseAuth
 
 class SignUpViewController: UIViewController {
     
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var phoneTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var submitButton: UIImageView!
     
+    var isSignIn: Bool = true
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func submitButtonTapped(_ sender: Any) {
         guard let name = nameTextField.text, let phone = phoneTextField.text, let email = emailTextField.text else { return }
         
-        
+        if isSignIn {
+            
+        }
         
         UserController.shared.createUser(name: name, phone: phone, email: email, isBroker: false /* <- just for committing purposes*/ )
     }
