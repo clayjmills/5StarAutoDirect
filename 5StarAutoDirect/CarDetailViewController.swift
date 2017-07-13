a0b1e2f19606a11effbe288dbd5e7a04307fb090
witty89 <witty89@gmail.com>
2017-07-13 10:25:27 -0600

    getting clay’s designs

--- 5StarAutoDirect/CarDetailViewController.swift
+++ 5StarAutoDirect/CarDetailViewController.swift
@@ -25,4 +25,18 @@ class CarDetailViewController: UIViewController {
         guard let make = makeTextField.text, let model = modelTextField.text, let budget =  budgetTextField.text, let color = colorTextField.text, let other = otherTextField.text else { return }
         CarController.shared.createCar(make: make, model: model, budget: budget, color: color, other: other)
     }
+    
+    func presentTextFieldsAreEmptyAlert() {
+        let emptyTextFieldsAlertController = UIAlertController(title: "One or more text fields are still empty", message: "Submit anyways?", preferredStyle: .alert)
+        let dismissAction = UIAlertAction(title: "Add", style: .cancel, handler: nil)
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { (createCar) in
+            CarController.shared.createCar(make: make, model: model, budget: budget, color: color, other: other)
+        }
+    }
 }
+
+
+
+
+
+
