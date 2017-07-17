f0def28f44d49c80a18d79e3252d6d7da3c469aa
clayjmills <claymills5@gmail.com>
2017-07-17 12:49:49 -0600

    Adding alerts for empty textfields in carDetailVC

--- 5StarAutoDirect/CarDetailViewController.swift
+++ 5StarAutoDirect/CarDetailViewController.swift
@@ -9,13 +9,16 @@
 import UIKit
 import Firebase
 
-class CarDetailViewController: UIViewController {
+class CarDetailViewController: UIViewController, UITextFieldDelegate {
     
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var makeTextField: UITextField!
     @IBOutlet weak var modelTextField: UITextField!
     @IBOutlet weak var budgetTextField: UITextField!
     @IBOutlet weak var colorTextField: UITextField!
     @IBOutlet weak var otherTextField: UITextField!
+    
+    var carCreated: Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,31 +26,97 @@ class CarDetailViewController: UIViewController {
     
     @IBAction func submitButtonTapped(_ sender: Any) {
         guard let make = makeTextField.text, let model = modelTextField.text, let budget =  budgetTextField.text, let color = colorTextField.text, let other = otherTextField.text else { return }
-
-        // TODO: - check if text fields are empty
-        // if {
-        textFieldsEmptyAlertController()
-        // } else {
-        CarController.shared.createCar(make: make, model: model, budget: budget, color: color, other: other)
-    //}
+        
+        if carCreated {
+        
+            if make == "" {
+                self.emptyMake()
+            }
+            if model == "" {
+                self.emptyModel()
+            }
+            if budget == "" {
+                self.emptyBudget()
+            }
+            if color == "" {
+                self.emptyColor()
+            }
+            if other == "" {
+                self.emptyOther()
+            }
+        }
+//
+//        // TODO: - check if text fields are empty
+//        // if {
+//        textFieldsEmptyAlertController()
+//        // } else {
+//        CarController.shared.createCar(make: make, model: model, budget: budget, color: color, other: other)
+//    //}
     
     }
     
-    func textFieldsEmptyAlertController() {
-        guard let make = makeTextField.text, let model = modelTextField.text, let budget =  budgetTextField.text, let color = colorTextField.text, let other = otherTextField.text else { return }
-        let emptyTextFieldsAlertController = UIAlertController(title: "Fill out all fields", message: "Thank you", preferredStyle: .alert)
-        
+//    func textFieldsEmptyAlertController() {
+//        guard let make = makeTextField.text, let model = modelTextField.text, let budget =  budgetTextField.text, let color = colorTextField.text, let other = otherTextField.text else { return }
+//        let emptyTextFieldsAlertController = UIAlertController(title: "Fill out all fields", message: "Thank you", preferredStyle: .alert)
+//        
+//        let dismissAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+//        
+//        emptyTextFieldsAlertController.addAction(dismissAction)
+//        //emptyTextFieldsAlertController.addAction(submitAction)
+//        present(emptyTextFieldsAlertController, animated: true, completion: nil)
+//    }
+    
+    func emptyMake() {
+        let emptyMakeAlertController = UIAlertController(title: "Please enter a make", message: nil, preferredStyle: .alert)
         let dismissAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-        
-        // To Do: delete if never used again
-        //let submitAction = UIAlertAction(title: "Submit", style: .default) { (createCar) in
-           // CarController.shared.createCar(make: make, model: model, budget: budget, color: color, other: other)
-        //}
-        
-        emptyTextFieldsAlertController.addAction(dismissAction)
-        //emptyTextFieldsAlertController.addAction(submitAction)
-        present(emptyTextFieldsAlertController, animated: true, completion: nil)
+        emptyMakeAlertController.addAction(dismissAction)
+        present(emptyMakeAlertController, animated: true, completion: nil)
     }
     
+    func emptyModel() {
+        let emptyModelAlertController = UIAlertController(title: "Please enter a model", message: nil, preferredStyle: .alert)
+        let dismissAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        emptyModelAlertController.addAction(dismissAction)
+        present(emptyModelAlertController, animated: true, completion: nil)
+    }
+    
+    func emptyBudget() {
+        let emptyBudgetAlertController = UIAlertController(title: "Please enter a budget", message: nil, preferredStyle: .alert)
+        let dismissAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        emptyBudgetAlertController.addAction(dismissAction)
+        present(emptyBudgetAlertController, animated: true, completion: nil)
+    }
+    
+    func emptyColor() {
+        let emptyColorAlertController = UIAlertController(title: "Please enter a color", message: nil, preferredStyle: .alert)
+        let dismissAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        emptyColorAlertController.addAction(dismissAction)
+        present(emptyColorAlertController, animated: true, completion: nil)
+    }
+    
+    func emptyOther() {
+        let emptyOtherAlertController = UIAlertController(title: "Please type NA if other is not applicable", message: nil, preferredStyle: .alert)
+        let dismissAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        emptyOtherAlertController.addAction(dismissAction)
+        present(emptyOtherAlertController, animated: true, completion: nil)
+    }
+    
+    // keep textfields on top of keyboard
+    
+    // TODO: customize each text field to stay in view when keyboard is up
+    // TODO: customize each keyboard (take away predictive text options)
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        scrollView.setContentOffset(CGPoint(x: 0, y:150), animated: true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        scrollView.setContentOffset(CGPoint(x: 0, y:0), animated: true)
+    }
     
 }
