1062901494d0ac911f9e7244552d25160f33a861
MikeDCastillo <mikecastillouvu@gmail.com>
2017-08-08 23:47:13 -0600

    Refactors code in Car Detail Vc

--- 5StarAutoDirect/CarDetailViewController.swift
+++ 5StarAutoDirect/CarDetailViewController.swift
@@ -25,6 +25,7 @@ class CarDetailViewController: UIViewController, UITextFieldDelegate {
     static let shared = CarDetailViewController()
     
     var carCreated: Bool = true
+    var isHighlighted = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,7 +45,7 @@ class CarDetailViewController: UIViewController, UITextFieldDelegate {
     var user: User?
     
     @IBAction func submitButtonTapped(_ sender: Any) {
-        
+    
         
         guard let make = makeTextField.text, let model = modelTextField.text, let budget =  budgetTextField.text, let color = colorTextField.text, let other = otherTextField.text else { return }
         
@@ -73,37 +74,78 @@ class CarDetailViewController: UIViewController, UITextFieldDelegate {
             }
         }
     }
+    
+    // MARK: - Textfield border color change functions
+    
+    func changeToRed() {
+        let redCGColor = UIColor.red.cgColor
+        
+        switch UITextField() {
+        case makeTextField:
+            makeTextField.layer.borderColor = redCGColor
+            makeTextField.layer.borderWidth = 1.0
+            shake(textfieldLayer: makeTextField.layer)
+        case modelTextField:
+            modelTextField.layer.borderColor = redCGColor
+            modelTextField.layer.borderWidth = 1.0
+            shake(textfieldLayer: modelTextField.layer)
+        case budgetTextField:
+            budgetTextField.layer.borderColor = redCGColor
+            budgetTextField.layer.borderWidth = 1.0
+            shake(textfieldLayer: budgetTextField.layer)
+        case colorTextField:
+            colorTextField.layer.borderColor = redCGColor
+            colorTextField.layer.borderWidth = 1.0
+            shake(textfieldLayer: colorTextField.layer)
+        default:
+            print("Break Statement")
+            break
+        }
+    }
+    
+    // MARK: - Animation Functions
+    
+    fileprivate func shake(textfieldLayer: CALayer) {
+        let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        animation.duration = 0.6
+        animation.values = [-20.0, 20.0, -20.0, 20.0, -10.0, 10.0, -5.0, 5.0, 0.0 ]
+        textfieldLayer.add(animation, forKey: "shake")
+    }
+    
+    // MARK: - Alert Controller Functions
+    let emptyTextFieldController = UIAlertController(title: "", message: nil, preferredStyle: .alert)
+    let dismissAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+    
+    func presentAlertControllerWithDismiss(alertController: UIAlertController, action: UIAlertAction) {
+    present(alertController, animated: true, completion: nil)
+    alertController.addAction(dismissAction)
+    }
+    
+    
     func emptyMake() {
-        let emptyMakeAlertController = UIAlertController(title: "Please enter a make", message: nil, preferredStyle: .alert)
-        let dismissAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-        emptyMakeAlertController.addAction(dismissAction)
-        present(emptyMakeAlertController, animated: true, completion: nil)
+        
+        let emptyMakeAlertController = UIAlertController(title: AlertText.make.title, message: nil, preferredStyle: .alert)
+       presentAlertControllerWithDismiss(alertController: emptyMakeAlertController, action: dismissAction)
     }
     
     func emptyModel() {
-        let emptyModelAlertController = UIAlertController(title: "Please enter a model", message: nil, preferredStyle: .alert)
-        let dismissAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-        emptyModelAlertController.addAction(dismissAction)
-        present(emptyModelAlertController, animated: true, completion: nil)
+        let emptyModelAlertController = UIAlertController(title: AlertText.model.title, message: nil, preferredStyle: .alert)
+       presentAlertControllerWithDismiss(alertController: emptyModelAlertController, action: dismissAction)
     }
     
     func emptyBudget() {
-        let emptyBudgetAlertController = UIAlertController(title: "Please enter a budget", message: nil, preferredStyle: .alert)
-        let dismissAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-        emptyBudgetAlertController.addAction(dismissAction)
-        present(emptyBudgetAlertController, animated: true, completion: nil)
+        let emptyBudgetAlertController = UIAlertController(title: AlertText.budget.title, message: nil, preferredStyle: .alert)
+        presentAlertControllerWithDismiss(alertController: emptyBudgetAlertController, action: dismissAction)
     }
     
     func emptyColor() {
-        let emptyColorAlertController = UIAlertController(title: "Please enter a color", message: nil, preferredStyle: .alert)
-        let dismissAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-        emptyColorAlertController.addAction(dismissAction)
-        present(emptyColorAlertController, animated: true, completion: nil)
+        let emptyColorAlertController = UIAlertController(title: AlertText.color.title, message: nil, preferredStyle: .alert)
+        presentAlertControllerWithDismiss(alertController: emptyColorAlertController, action: dismissAction)
     }
-    
+    //FIXME: - finish this alert
     func emptyOther() {
-        let emptyOtherAlertController = UIAlertController(title: "Please type NA if other is not applicable", message: nil, preferredStyle: .alert)
-        let dismissAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        let emptyOtherAlertController = UIAlertController(title: AlertText.other.message, message: nil, preferredStyle: .alert)
         emptyOtherAlertController.addAction(dismissAction)
         present(emptyOtherAlertController, animated: true, completion: nil)
     }
@@ -112,6 +154,7 @@ class CarDetailViewController: UIViewController, UITextFieldDelegate {
     
     // TODO: customize each text field to stay in view when keyboard is up
     // TODO: customize each keyboard (take away predictive text options)
+    // TODO: add inirializer for Car obj with textfield input
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
@@ -124,6 +167,40 @@ class CarDetailViewController: UIViewController, UITextFieldDelegate {
     
     func textFieldDidEndEditing(_ textField: UITextField) {
         scrollView.setContentOffset(CGPoint(x: 0, y:0), animated: true)
+    }
+    
+}
+
+enum AlertText: String {
+    
+    case make
+    case model
+    case budget
+    case color
+    case other
+    
+    var title: String? {
+        switch self {
+        case .make:
+           return "Please enter a make"
+        case .model:
+            return "Please enter a model"
+        case .budget:
+            return"Please enter a budget"
+        case .color:
+            return "Please enter a color"
+        default:
+            return nil
+        }
+    }
+    
+    var message: String? {
+        switch self {
+        case .other:
+            return "Would you like to add any other notes before moving on?"
+        default:
+            return nil
+        }
     }
     
 }
