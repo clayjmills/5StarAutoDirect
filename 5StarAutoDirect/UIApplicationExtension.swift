28c3d6fa573306f87be62c4adf249300705cd5c3
MikeDCastillo <mikecastillouvu@gmail.com>
2017-08-28 09:40:25 -0600

    Adds files to help detect topmost VC

--- 5StarAutoDirect/UIApplicationExtension.swift
+++ 5StarAutoDirect/UIApplicationExtension.swift
@@ -6,39 +6,24 @@
 //  Copyright © 2017 PineAPPle LLC. All rights reserved.
 // https://stackoverflow.com/questions/26667009/get-top-most-uiviewcontroller
 
-import Foundation
+import UIKit
+// FIXME: - This is breaking MVC. future update, utilize protocols to promote good architecture
 
-extension UIWindow {
+// implementation : if let topController = UIApplication.topViewController() {}
+extension UIApplication {
     
-    
-    func visibleViewController() -> UIViewController? {
-        if let rootViewController: UIViewController  = self.rootViewController {
-            return UIWindow.getVisibleViewControllerFrom(rootViewController)
+    class func topViewController(controller: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        if let navigationController = controller as? UINavigationController {
+            return topViewController(controller: navigationController.visibleViewController)
         }
-        return nil
-    }
-    
-    class func getVisibleViewControllerFrom(vc:UIViewController) -> UIViewController {
-        
-        if vc.isKindOfClass(UINavigationController.self) {
-            
-            let navigationController = vc as UINavigationController
-            return UIWindow.getVisibleViewControllerFrom( navigationController.visibleViewController)
-            
-        } else if vc.isKindOfClass(UITabBarController.self) {
-            
-            let tabBarController = vc as UITabBarController
-            return UIWindow.getVisibleViewControllerFrom(tabBarController.selectedViewController!)
-            
-        } else {
-            
-            if let presentedViewController = vc.presentedViewController {
-                
-                return UIWindow.getVisibleViewControllerFrom(presentedViewController!)
-                
-            } else {
-                
-                return vc;
+        if let tabController = controller as? UITabBarController {
+            if let selected = tabController.selectedViewController {
+                return topViewController(controller: selected)
             }
         }
+        if let presented = controller?.presentedViewController {
+            return topViewController(controller: presented)
+        }
+        return controller
+    }
 }
--- 5StarAutoDirect/ViewControllerHelper.swift
+++ 5StarAutoDirect/ViewControllerHelper.swift
@@ -6,7 +6,19 @@
 //  Copyright © 2017 PineAPPle LLC. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import Whisper
 
-struct 
+extension UIViewController {
+// broker tv, car detail, messageconvo collection, user tvc, userinfo tvc
+    func showNotificationBanner() {
+        let announcement = Announcement(title: "New Message Received", subtitle: "Click here to go to messages", image: nil, action: nil)
+        if let topController = UIApplication.topViewController(){
+            Whisper.show(shout: announcement, to: topController)
+        }
+    }
+}
+
+protocol currentView {
+    var currentView: UIView { get }
+}
